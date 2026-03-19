fc767420760da4171589e28c3c1a78a3d1158a3a
Omkar-Gawali <omkargawali702@gmail.com>
2026-03-19 12:14:13 +0530

    fix api baseurl for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem("resumeUser");
